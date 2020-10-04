b787b39c517d9fa1370383056a02e347c2bf5f1c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-04 22:14:18 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,6 +11,9 @@ class ArtistsController < ApplicationController
   end
 
   def update
+    @artist = Aartist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
   end
 
   def show
